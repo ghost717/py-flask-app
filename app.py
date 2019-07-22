dbbf16205233dec5272ec09d011b1b084a4b9347
ghost717 <dawid.zawadzki7717@gmail.com>
2019-07-22 21:43:39 +0200

    signin

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, redirect, session
 from flaskext.mysql import MySQL
 # import MySQLdb
 from werkzeug import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
-
 mysql = MySQL()
- 
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -23,7 +24,62 @@ def main():
 def showSignUp():
     return render_template('signup.html')
 
-@app.route('/signUp',methods=['POST, GET'])
+@app.route('/showSignin')
+def showSignin():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('signin.html')
+
+@app.route('/userHome')
+def userHome():
+    if session.get('user'):
+        return render_template('userHome.html')
+    else:
+        return render_template('error.html',error = 'Unauthorized Access')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+
+@app.route('/validateLogin',methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+        
+
+        
+        # connect to mysql
+
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+
+        
+
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+            
+
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+
+
+@app.route('/signUp',methods=['POST','GET'])
 def signUp():
     try:
         _name = request.form['inputName']
@@ -54,6 +110,8 @@ def signUp():
     finally:
         cursor.close() 
         conn.close()
+
+
 
 if __name__ == "__main__":
     app.run()
