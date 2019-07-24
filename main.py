753b5e55357bba90cf6e72722947c3589b878692
ghost717 <dawid.zawadzki7717@gmail.com>
2019-07-24 23:00:08 +0200

    test

--- main.py
+++ main.py
@@ -82,6 +82,27 @@ def send_image(filename):
     # print(image["filename"])
     return send_from_directory('static/temp', filename)
 
+# @app.route('/', defaults={'req_path': ''})
+# @app.route('/<path:req_path>')
+# def dir_listing(req_path):
+# 	APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+# 	BASE_DIR = os.path.join(APP_ROOT, 'static/')
+
+#     # Joining the base and the requested path
+#     abs_path = os.path.join(BASE_DIR, req_path)
+
+#     # Return 404 if path doesn't exist
+#     if not os.path.exists(abs_path):
+#         return abort(404)
+
+#     # Check if path is a file and serve
+#     if os.path.isfile(abs_path):
+#         return send_file(abs_path)
+
+#     # Show directory contents
+#     files = os.listdir(abs_path)
+#     return render_template('files.html', files=files)
+
 @app.route('/edit/<int:id>')
 def edit_view(id):
 	try:
