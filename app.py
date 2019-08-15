6070ac1e6e49f01b3c1c57d454497a4ce93636ab
ghost717 <dawid.zawadzki7717@gmail.com>
2019-08-15 14:57:50 +0200

    cleaning

--- app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-import os
-
-app = Flask(__name__)
-app.secret_key = "secret key"
-
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-app.config["TEST_PATH"] = os.path.join(APP_ROOT, 'static/')
